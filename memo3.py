df2fbc389c7cb2385b92bd14615416a71ac67639
kimbohyeun <107673649+kimbohyeun@users.noreply.github.com>
2022-07-27 17:49:49 +0900

    메모장만들기.읽기기능

---
+++ memo3.py
@@ -0,0 +1,18 @@
+print("hello world")
+print("진행할 작업을 선택해주세요")
+print("1. 메모작업 2.메모읽기")
+
+option = input()
+
+print(option)
+
+if option == '1':
+    print("아직 구현안함")
+elif option =="2":
+    f = open("memo.txt")
+    memo = f.read()
+    print(type(memo))
+    print(memo)
+    f.close
+else:
+    print("잘못 입력 했습니다.")
